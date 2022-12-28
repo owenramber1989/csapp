aafa1d0121469d71c3a105e90be4a701e9c1b8fe
owenramber1989 <owenramber1989@163.com>
2022-12-28 15:20:32 +0800

    Implement tmin function

--- datalab-handout/bits.c
+++ datalab-handout/bits.c
@@ -148,7 +148,7 @@ int bitXor(int x, int y) { return ~(~x & ~y) & ~(x & y); }
  *   Max ops: 4
  *   Rating: 1
  */
-int tmin(void) { return 2; }
+int tmin(void) { return 1 << 31; }
 // 2
 /*
  * isTmax - returns 1 if x is the maximum, two's complement number,
