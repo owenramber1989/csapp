24f78a16eb7cf01040ea0cc8d85e3f2850754add
owenramber1989 <owenramber1989@163.com>
2022-12-28 15:13:43 +0800

    Implement the bitxor function.

--- datalab-handout/bits.c
+++ datalab-handout/bits.c
@@ -141,7 +141,7 @@ NOTES:
  *   Max ops: 14
  *   Rating: 1
  */
-int bitXor(int x, int y) {}
+int bitXor(int x, int y) { return ~(~x & ~y) & ~(x & y); }
 /*
  * tmin - return minimum two's complement integer
  *   Legal ops: ! ~ & ^ | + << >>
